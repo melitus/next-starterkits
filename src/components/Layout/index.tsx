dba4b7d4e11b39f166888608644df018b57b9497
melitus <asmelitus@gmail.com>
2021-04-12 13:12:35 +0100

    feat: add footer component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,7 +3,7 @@ import React, {ReactNode} from 'react'
 import Header from '../Header'
 import Head from './Head'
 import {ILayout} from './Layout'
-
+import {Footer} from '../Footer'
 
 
 const Layout:React.FunctionComponent<ILayout.IProps> = (props): JSX.Element => {
@@ -13,6 +13,7 @@ const Layout:React.FunctionComponent<ILayout.IProps> = (props): JSX.Element => {
       <div>
         <Header />
         {props.children}
+        <Footer />
       </div>
     </>
   )
